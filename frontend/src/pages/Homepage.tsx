4d806f67429177abfced35a6717980385d05dba1
Manan <Mananbhardwaj2705@gmail.com>
2025-11-05 22:34:59 +0530

    Added git api data

--- frontend/src/pages/Homepage.tsx
+++ frontend/src/pages/Homepage.tsx
@@ -1,11 +1,49 @@
-
+import Search from "../components/Search";
+import SortRepos from "../components/Sortrepo";
+import Repos from "../components/Repos";
+import ProfileInfo from "../components/Profile";
+import Spinner from "../components/Spinner";
+import { useCallback, useEffect, useState } from "react";
+import axios from "axios";
 
 const Homepage = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+const [userProfile, setUserProfile] = useState<any>("");
+const [repos, setRepos] = useState<any>([]);
+const [loading, setLoading] = useState(false);
+const [sortRepo, setSortRepo] = useState('forked');
 
-export default Homepage
+
+const getUserProfileAndRepo = useCallback(async ()=>{
+	setLoading(true);
+	try {
+		const res = await axios.get(`https://api.github.com/users/https-manan`);
+		const userProfile = res.data;
+		setUserProfile(userProfile);
+		const repores = await axios.get(userProfile?.repos_url);
+		const repos = repores.data;
+		setRepos(repos);
+	} catch (error) {
+		console.log(error);
+	} finally {
+		setLoading(false);
+	}
+},[])
+
+useEffect(() => {
+	getUserProfileAndRepo();
+}, []);
+
+return (
+	<div className='m-4'>
+		<Search />
+		<SortRepos />
+		<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
+			<ProfileInfo userProfile={userProfile}/>
+			<Repos />
+			<Spinner />
+		</div>
+	</div>
+);
+};
+
+export default Homepage;
